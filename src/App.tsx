5bcef06235c5cf305e63334c1b0f0e0bdae520b6
Ellen-Iconoclust <elleniconoclust@gmail.com>
2026-04-26 10:08:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -101,7 +101,7 @@ export default function App() {
               <div className="mt-4 md:mt-0">
                 <p className="text-xs text-gray-500 flex items-center gap-1">
                   <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-code"><polyline points="16 18 22 12 16 6"/><polyline points="8 6 2 12 8 18"/></svg>
-                  Platform Version: Xam4.0
+                  Platform Version: Xam5.0
                 </p>
               </div>
             </div>
